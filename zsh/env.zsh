f8040593974c50b7878dce646c0f84b06855b10a
fan xian <fxean@icloud.com>
2025-10-21 19:04:30 +0800

    scripts path env

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -12,5 +12,6 @@ export http_proxy=http://127.0.0.1:7897
 export https_proxy=http://127.0.0.1:7897
 
 export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/dotfiles/scripts:$PATH"
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_ENV_HINTS=1
