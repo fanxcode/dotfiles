26694feffa74c4f451a7b78ce41d10049e2da723
fan xian <fxean@icloud.com>
2025-11-07 00:14:41 +0800

    add swift treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "vim", "bash", "c", "cpp", "javascript", "json", "lua", "python"},
+  ensure_installed = { "vim", "swift", "bash", "c", "cpp", "javascript", "json", "lua", "python"},
   highlight = { enable = true },
   indent = { enable = true },
 
