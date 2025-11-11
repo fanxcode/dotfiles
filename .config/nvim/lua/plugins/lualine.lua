8259116365d856b3918b4742dadc76c8ca81d083
fan xian <fxean@icloud.com>
2025-11-11 08:20:58 +0800

    lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto", -- 自动适配主题
+        globalstatus = true, -- 让底部整条都是状态栏（更好看）
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}
