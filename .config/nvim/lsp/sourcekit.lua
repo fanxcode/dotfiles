da5c151bda23cd2783cf0d559f4fe95f3d89386a
fan xian <fxean@icloud.com>
2025-11-17 20:27:24 +0800

    let sourcekit see Package.swift

--- .config/nvim/lsp/sourcekit.lua
+++ .config/nvim/lsp/sourcekit.lua
@@ -1,4 +1,21 @@
 return {
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+
   cmd = { 'sourcekit-lsp' },
+
   filetypes = { 'swift' },
+
+  root_markers = {
+    'Package.swift',
+    '.git',
+  },
+
+  single_file_support = false,
 }
